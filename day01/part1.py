f5bf5f3cf4896ec23afeabc6ac61bcb77f81503b
Ian Contijoch <ian.contijoch@gmail.com>
2026-01-10 17:25:41 -0500

    first commit + day1-part1

---
+++ day01/part1.py
@@ -0,0 +1,52 @@
+from __future__ import annotations
+
+import argparse
+import os.path
+
+import pytest
+
+import support
+
+INPUT_TXT = os.path.join(os.path.dirname(__file__), 'input.txt')
+
+
+def compute(s: str) -> int:
+    lines = s.splitlines()
+    total = 0
+    for line in lines:
+        nums_s = line.split(', ')
+        for n_s in nums_s:
+            sign = 1 if n_s[0] == '+' else -1
+            total += sign * int(n_s[1:])
+    return total
+
+
+INPUT_S = '''\
++1, -2, +3, +1
+'''
+EXPECTED = 3
+
+
+@pytest.mark.parametrize(
+    ('input_s', 'expected'),
+    (
+        (INPUT_S, EXPECTED),
+    ),
+)
+def test(input_s: str, expected: int) -> None:
+    assert compute(input_s) == expected
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('data_file', nargs='?', default=INPUT_TXT)
+    args = parser.parse_args()
+
+    with open(args.data_file) as f, support.timing():
+        print(compute(f.read()))
+
+    return 0
+
+
+if __name__ == '__main__':
+    raise SystemExit(main())
